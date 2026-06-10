22029528dbae1ed293aa59e7cf3a2745359277f8
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-06-10 15:27:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (36.95%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
     vector<int> findAnagrams(string txt, string pat) {
-        int n = txt.length();
-        int m = pat.length();
+        int n = txt.length(), m = pat.length();
         vector<int> result;
-        vector<int> pat_freq(256, 0);
-        vector<int> txt_freq(256, 0);
-        if(n < m) return result;
-        for(int i=0; i<m; i++)
-        {
+        if (n < m)
+            return result;
+
+        vector<int> pat_freq(256, 0), txt_freq(256, 0);
+
+        // Pehli window ke characters count store kiye
+        for (int i = 0; i < m; i++) {
             pat_freq[pat[i]]++;
             txt_freq[txt[i]]++;
         }
 
-        for(int i=m; i<=n; i++)
-        {
-            if(pat_freq == txt_freq) result.push_back(i-m);
-            txt_freq[txt[i]]++;
-            txt_freq[txt[i-m]]--;
-        }
-        // if(pat_freq == txt_freq) result.push_back(n-m);
+        // Loop ko 'n' tak chalaya taaki aakhri check miss na ho
+        for (int i = m; i <= n; i++) {
 
+            // Purna check loop ke starting mein hi ho jayega (including last
+            // window)
+            if (pat_freq == txt_freq) {
+                result.push_back(i - m);
+            }
+
+            // SAFE CHECK: Agar i == n ho gaya hai, toh naya char add mat karo
+            // (string khatam)
+            if (i < n) {
+                txt_freq[txt[i]]++;     // Naya character window mein aaya
+                txt_freq[txt[i - m]]--; // Purana character window se gaya
+            }
+        }
 
         return result;
     }
-    // vector<int> findAnagrams(string txt, string pat) {
-    //     int n = txt.length();
-    //     int m = pat.length();
-    //     vector<int> result;
-    //     if(n < m) return result;
-    //     sort(pat.begin(), pat.end());
-    //     for(int i=0; i<=n-m; i++)
-    //     {
-    //         string window = txt.substr(i, m);
-    //         sort(window.begin(), window.end());
-    //         if(window == pat) result.push_back(i);
-    //     }
-    //     return result;
-    // }
 };
