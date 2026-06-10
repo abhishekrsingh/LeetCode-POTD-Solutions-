32aa1a6b33908c7d2c53a08ae805ed893c24b0b0
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-06-10 18:26:23 +0530

    Time: 7 ms (19.18%) | Memory: 9.8 MB (49.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
+    // Bruteforce approach
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        vector<int> s_freq(256, 0);
-        vector<int> t_freq(256, 0);
-        for (int i = 0; i < s.length(); i++) {
-            s_freq[s[i]]++;
-            t_freq[t[i]]++;
-        }
-        if(s_freq == t_freq) return true;
-        else return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
     }
 };
