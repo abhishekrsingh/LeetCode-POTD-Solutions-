a1f6cb63fec56cddec501babde313d041abd1ab8
Abhishek Kumar <91167858+abhishekrsingh@users.noreply.github.com>
2026-09-10 07:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (63.85%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 1) return true;
+        for(int i=1; i<n; i++){
+            if(arr[i]%2 == arr[i-1]%2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
